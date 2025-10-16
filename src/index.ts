482d64bc50fef17b9b7932ab32c24fb3f1c037b2
Yiğit Önay <mavi221907@gmail.com>
2025-10-16 09:07:04 +0300

    tryforfin
    tryforfin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // Rotaları içeri aktar
 import story_routes from "./routes/story_routes.js";
@@ -17,22 +15,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
 // Rotaları kullan
 app.use("/api/story", story_routes);
 app.use("/api/auth", auth_routes);
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-});
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
